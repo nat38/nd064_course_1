f81e1a974af55c1db3cf823665198f8c59de85d5
nat389 <n.seela@gmail.com>
2022-04-18 21:51:59 +0200

    added docker file and finalized app

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,6 +2,11 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+import sys
+
+app = Flask(__name__)
+log_format = "%(levelname)s:%(name)s:%(asctime)s - %(message)s"
 
 db_connection_count = 0
 
@@ -38,6 +43,7 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application 
 @app.route('/')
 def index():
+    app.logger.info('Index request successfull')
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
@@ -51,16 +57,19 @@ def post(post_id):
     if post is None:
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article ' + post['title'] + ' retreived!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About request successfull')
     return render_template('about.html')
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    app.logger.info('Create request successfull')
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -81,6 +90,7 @@ def create():
 # Define the health check endpoint
 @app.route('/healthz')
 def healthz():
+    app.logger.info('Healthz request successfull')
     response = app.response_class(
             response=json.dumps({"result":"OK - healthy"}),
             status=200,
@@ -91,6 +101,7 @@ def healthz():
 # Define the metrics endpoint
 @app.route('/metrics')
 def metrics():
+    app.logger.info('Metrics request successfull')
     response = app.response_class(
             response=json.dumps({"db_connection_count": db_connection_count, "post_count": get_post_count()}),
             status=200,
@@ -101,4 +112,7 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(stream = sys.stdout,
+                    format = log_format,
+                    level=logging.INFO)
    app.run(host='0.0.0.0', port='3111')
